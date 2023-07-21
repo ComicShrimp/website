f50a3435a5dcfde31e9b59dfe48110bce5a2f925
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2023-07-21 16:36:14 -0300

    chore: basic info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-1">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold">Hello, world!</h1>
-        <p className="text-center text-2xl">Welcome to your new Next.js app.</p>
+        <h1 className="text-6xl font-bold">Mário Victor</h1>
+        <p className="text-center text-2xl">Software Developer</p>
       </div>
     </main>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header className="flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">Mário Victor</h1>
+      <p className="text-center text-2xl">Software Developer</p>
+    </header>
+  )
+}
