471012dbb59053511abba97baa807c955aecda86
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2023-08-31 17:05:25 -0300

    feat: profile picture

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   eslint: {
     dirs: ['src'],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-1">
       <div className="flex flex-col items-center justify-center">
+        <Image
+          className="rounded-full"
+          src="https://avatars.githubusercontent.com/u/43503750"
+          width={250}
+          height={250}
+          alt="Profile picture"
+        />
         <h1 className="text-6xl font-bold">Mário Victor</h1>
         <p className="text-center text-2xl">Software Developer</p>
       </div>
