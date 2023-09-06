83b981efb89207a9ea7cffe38423dbdc6e798506
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2023-09-06 11:24:49 -0300

    feat: add links to social

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import githubIcon from '../assets/github-icon.svg'
 import linkedinIcon from '../assets/linkedin-icon.svg'
 import mediumIcon from '../assets/medium-icon.svg'
+import Link from 'next/link'
 
 type Props = {
   iconWidth?: number
@@ -11,24 +12,30 @@ type Props = {
 export function SocialIcons({ iconWidth = 50, iconHeight = 50 }: Props) {
   return (
     <div className="flex items-center justify-center space-x-4">
-      <Image
-        src={githubIcon}
-        width={iconWidth}
-        height={iconHeight}
-        alt="Github Logo"
-      />
-      <Image
-        src={linkedinIcon}
-        width={iconWidth}
-        height={iconHeight}
-        alt="Linkedin Logo"
-      />
-      <Image
-        src={mediumIcon}
-        width={iconWidth}
-        height={iconHeight}
-        alt="Medium Logo"
-      />
+      <Link href="https://github.com/ComicShrimp" target="_blank">
+        <Image
+          src={githubIcon}
+          width={iconWidth}
+          height={iconHeight}
+          alt="Github Logo"
+        />
+      </Link>
+      <Link href="https://www.linkedin.com/in/mariovictorrs" target="_blank">
+        <Image
+          src={linkedinIcon}
+          width={iconWidth}
+          height={iconHeight}
+          alt="Linkedin Logo"
+        />
+      </Link>
+      <Link href="https://medium.com/@mariovictorrs" target="_blank">
+        <Image
+          src={mediumIcon}
+          width={iconWidth}
+          height={iconHeight}
+          alt="Medium Logo"
+        />
+      </Link>
     </div>
   )
 }
